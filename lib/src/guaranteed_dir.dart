553395704c64296760a6d76e7d5ba09230bb6f12
L3odr0id <leon.leonc@yandex.ru>
2022-03-03 15:31:13 +0300

    Optimization

--- lib/src/guaranteed_dir.dart
+++ lib/src/guaranteed_dir.dart
@@ -1,7 +1,5 @@
 import 'dart:io';
 
-import 'package:keystore_generator/src/message.dart';
-
 class GuaranteedDir {
   const GuaranteedDir({
     required this.dirName,
@@ -12,16 +10,6 @@ class GuaranteedDir {
   final bool needAlert;
 
   void makeDir() {
-    final dir = Directory(dirName);
-    if (!dir.existsSync()) {
-      if (needAlert) {
-        throw MessageError(
-          info:
-              '$dirName folder was not found in ${Directory.current.absolute.path}',
-        ).decoratedMessage();
-      } else {
-        dir.createSync();
-      }
-    }
+    Directory(dirName).createSync();
   }
 }
